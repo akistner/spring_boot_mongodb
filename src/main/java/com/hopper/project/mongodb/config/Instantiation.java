c3a01e7a523e4da667c41e32074e0d8f7b6a887f
Anderson Kistner <andersonkistner@hotmail.com>
2020-07-21 17:26:38 -0300

    Dados do autor projetados com DTO

--- src/main/java/com/hopper/project/mongodb/config/Instantiation.java
+++ src/main/java/com/hopper/project/mongodb/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.hopper.project.mongodb.domain.Post;
 import com.hopper.project.mongodb.domain.User;
+import com.hopper.project.mongodb.dto.AuthorDTO;
 import com.hopper.project.mongodb.repositories.PostRepository;
 import com.hopper.project.mongodb.repositories.UserRepository;
 
@@ -24,7 +25,6 @@ public class Instantiation implements CommandLineRunner {
 	
 	@Override
 	public void run(String... arg0) throws Exception {
-		
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 		
@@ -35,10 +35,11 @@ public class Instantiation implements CommandLineRunner {
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
-		
 		userReposiroty.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		
 		postReposiroty.saveAll(Arrays.asList(post1, post2));
 	}
 	
