62f162f297a50a5869dcda27f6b86c864830b070
Anderson Kistner <andersonkistner@hotmail.com>
2020-07-22 18:09:22 -0300

    Consulta simples com query methods implementada

--- src/main/java/com/hopper/project/mongodb/repositories/PostRepository.java
+++ src/main/java/com/hopper/project/mongodb/repositories/PostRepository.java
@@ -1,5 +1,7 @@
 package com.hopper.project.mongodb.repositories;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,7 @@ import com.hopper.project.mongodb.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+	
+	List<Post> findByTitleContainingIgnoreCase(String text);
+	
 }
--- src/main/java/com/hopper/project/mongodb/resources/PostResource.java
+++ src/main/java/com/hopper/project/mongodb/resources/PostResource.java
@@ -1,13 +1,17 @@
 package com.hopper.project.mongodb.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hopper.project.mongodb.domain.Post;
+import com.hopper.project.mongodb.resources.utils.URL;
 import com.hopper.project.mongodb.services.PostService;
 
 @RestController
@@ -21,6 +25,13 @@ public class PostResource {
 	public ResponseEntity<Post> findById(@PathVariable String id) {
 		Post obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
 	}
 	
 }
