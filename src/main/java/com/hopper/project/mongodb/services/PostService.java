0e3929c8e149fda0bd313fd461120ac09f5abe97
Anderson Kistner <andersonkistner@hotmail.com>
2020-07-22 16:46:59 -0300

    Post por id obtido

---
+++ src/main/java/com/hopper/project/mongodb/services/PostService.java
@@ -0,0 +1,23 @@
+package com.hopper.project.mongodb.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.hopper.project.mongodb.domain.Post;
+import com.hopper.project.mongodb.repositories.PostRepository;
+import com.hopper.project.mongodb.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repository;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+}
